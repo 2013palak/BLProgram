68ea0e4ae9a93666828ac5c92b5a9d75db716298
@2013palak <palak2013.be22@chitkara.edu.in>
2025-07-15 12:45:00 +0530

    adding new java file

---
+++ java2/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Please enter your basic details ");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = sc.nextLine();
+        System.out.println(name);
+
+        System.out.println("Please enter your roll no: ");
+        Long roll_no = sc.nextLong();
+        System.out.println(roll_no);
+        System.out.println(sc.nextLine());
+
+        System.out.println("Please enter your university email: ");
+        String email = sc.nextLine();
+        System.out.println(email);
+    }
+}
