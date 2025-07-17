93790f26df25574a49338bc6a0584f33a1b503aa
@2013palak <palak2013.be22@chitkara.edu.in>
2025-07-17 10:08:10 +0530

    17 july

--- java2/src/ternary.java
+++ java2/src/ternary.java
@@ -2,22 +2,52 @@ import java.util.Scanner;
 
 public class ternary {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter first number (a): ");
+//        int a = scanner.nextInt();
+//
+//        System.out.print("Enter second number (b): ");
+//        int b = scanner.nextInt();
+//
+//        System.out.print("Enter third number (c): ");
+//        int c = scanner.nextInt();
+//
+//
+//        System.out.print("Enter fourth number (d): ");
+//        int d = scanner.nextInt();
+//
+//
+//        int max = (a > b)
+//                ? ((a > c)
+//                ? ((a > d) ? a : d)
+//                : ((c > d) ? c : d))
+//                : ((b > c)
+//                ? ((b > d) ? b : d)
+//                : ((c > d) ? c : d));
+//
+//        System.out.println("The largest number is: " + max);
+//
+//        scanner.close();
 
-        System.out.print("Enter first number (a): ");
-        int a = scanner.nextInt();
-
-        System.out.print("Enter second number (b): ");
-        int b = scanner.nextInt();
-
-        System.out.print("Enter third number (c): ");
-        int c = scanner.nextInt();
 
 
-        int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
 
-        System.out.println("The largest number is: " + max);
 
-        scanner.close();
-    }
+//        int n;
+//        for(int i=2;i<=20;i++){
+//            if(i%2==0) {
+//                System.out.println(i);
+//            }
+//        }
+
+
+
+        int i=1;
+        int count=2;
+        while(i<=10) {
+            System.out.println(count*i);
+            i++;
+        }
+        }
 }
